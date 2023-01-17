dc3ef1a61692dfda59fd3f37e6fbdde5f5555df0
dberry38 <108836058+dberry38@users.noreply.github.com>
2023-01-17 13:48:10 -0800

    Develop index.jsx files for components

---
+++ forest-medallion/src/components/Nav/index.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+
+const Nav = (props) => {
+    const {
+        pages = [],
+        setCurrentPage,
+        currentPage,
+    } = props;
+
+    useEffect(() => {
+        document.title = currentPage.name;
+    }, [currentPage])
+
+  return (
+    <nav>
+        <ul>
+            {pages.map((Page) => (
+                <li className={`${currentPage.name === Page.name && 'navActive'}`} key={Page.name}>
+                    <span onClick={() => setCurrentPage(Page)}>
+                        {Page.name}
+                    </span>
+                </li>
+            ))}
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
---
+++ forest-medallion/src/components/PageContents/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const PageContents = (props) => {
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+}
+
+export default PageContents;
