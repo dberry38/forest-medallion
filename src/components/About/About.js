5abc598b29894c506d3847b1b465495d591eb2e2
David Berry <davidberry38@gmail.com>
2023-01-28 14:08:45 -0800

    Add template About page

---
+++ src/components/About/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function AboutDiv() {
+    return (
+        <div>
+            <hi>About Me And My Coding Skills Or Lack Thereof</hi>
+            <h2>Probably won't keep this second header</h2>
+            <h3>Or this one</h3>
+            <h4>But I'm on a roll with these, can't stop</h4>
+            <h5>Okay here's a list of places I want to travel to:</h5>
+            <ul>
+                <li>Iceland</li>
+                <li>New Zealand</li>
+                <li>Australia</li>
+                <li>Chile</li>
+                <li>Argentina</li>
+                <li>Thailand</li>
+            </ul>
+        </div>
+    );
+}
+
+export default AboutDiv;
